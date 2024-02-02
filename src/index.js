ad656838c075ec4f2c282291c647e68839a89c7e
ThaiHung <thaihung147741@gmail.com>
2024-02-02 22:26:05 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BaiTapThucHanhLayout from './components/BaiTapThucHanhLayout/BaiTapThucHanhLayout';
+
+
+
+
+// jsx: html được viết trong file js
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+Component:
+-Function component
+-Class component
+
+*/
+
+
+
+root.render(<div>
+   <BaiTapThucHanhLayout/>
+</div>);
+
